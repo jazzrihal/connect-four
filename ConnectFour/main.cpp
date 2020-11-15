f3689ab760fa8ad2cf459ad59e2290f059fb4802
jazzrihal <52720056+jazzrihal@users.noreply.github.com>
2020-11-15 15:01:55 +0000

    Major OOP refactor
    Object-oriented implementation ready for 'cartridge' game

--- ConnectFour/main.cpp
+++ ConnectFour/main.cpp
@@ -14,118 +14,119 @@ enum SlotState {
     PLAYER_2
 };
 
-void SetupBoard(int &height, int &width, SlotState **&board);
-void DrawBoard(int &height, int &width, SlotState **&board);
-void GetInput(int &width, int &selected);
-void PlaceToken(int &height,
-                int &width,
-                SlotState **&board,
-                SlotState &current_player,
-                int column);
-void SwitchPlayer(SlotState &current_player);
-void QuitGame(int & height, SlotState **&board);
+class Game {
+    virtual void Draw() = 0;
+    virtual void Input() = 0;
+};
 
-int main () {
-    cout << "Welcome to Connect Four!" << endl;
-    
-    // Setup game
-    int height = 6;
-    int width = 7;
+class ConnectFour : public Game {
+    int height, width;
     SlotState **board;
-    SetupBoard(height, width, board);
-    SlotState current_player = PLAYER_1;
-    int player_selected;
+    SlotState current_player;
+    int user_in;
     
-    DrawBoard(height, width, board);
-    GetInput(width, player_selected);
-    
-    // Game loop
-    while (player_selected) {
-        PlaceToken(height, width, board, current_player, player_selected);
-        SwitchPlayer(current_player);
-        DrawBoard(height, width, board);
-        GetInput(width, player_selected);
+public:
+    ConnectFour() {
+        // Implement user-defined board size here
+        height = 6;
+        width = 7;
+        
+        // Allocate memory
+        board = new SlotState*[height];
+        for (int i = 0; i < height; i++)
+            board[i] = new SlotState[width];
+        
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                board[i][j] = BLANK;
+        
+        // Prepare for first player
+        current_player = PLAYER_1;
+        user_in = 1;
     }
-
-    QuitGame(height, board);
-    return 0;
-}
-
-void SetupBoard(int &height, int &width, SlotState **&board) {
-    board = new SlotState*[height];
-    for (int i = 0; i < height; i++)
-        board[i] = new SlotState[width];
     
-    for (int i = 0; i < height; i++)
+    ~ConnectFour() {
+        // Deallocate memory
+        for (int i = 0; i < height; i++)
+            delete [] board[i];
+        delete [] board;
+    }
+    
+    void Draw() override {
+        // Print column header
         for (int j = 0; j < width; j++)
-            board[i][j] = BLANK;
-}
-
-void DrawBoard(int &height, int &width, SlotState **&board) {
-    for (int j = 0; j < width; j++)
-        cout << j + 1 << " ";
-    cout << endl;
-    
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            SlotState state = board[i][j];
-            switch (state)
-            {
-            case BLANK:     cout << "_ ";   break;
-            case PLAYER_1:  cout << "+ ";   break;
-            case PLAYER_2:  cout << "x ";   break;
-            default:        cout << "E";    break;
+            cout << j + 1 << " ";
+        cout << endl;
+        
+        // Print board in its current state
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                SlotState state = board[i][j];
+                switch (state)
+                {
+                case BLANK:     cout << "_ ";   break;
+                case PLAYER_1:  cout << "+ ";   break;
+                case PLAYER_2:  cout << "x ";   break;
+                default:        cout << "E";    break;
+                }
             }
+            cout << endl;
         }
         cout << endl;
     }
-    cout << endl;
-}
 
-void GetInput(int &width, int &selected) {
-    int user_in;
-    cin >> user_in;
-    
-    if (user_in >= 0 && user_in <= width) {
-        selected = user_in;
-    } else {
-        cout << "Enter number between 1 and " << width << endl;
-    }
-}
-
-void PlaceToken(int &height,
-                int &width,
-                SlotState **&board,
-                SlotState &current_player,
-                int column) {
-    
-    column--;
-    
-    for (int i = height - 1; i >= 0; i--) {
-        SlotState current_state = board[i][column];
-        if (current_state == BLANK) {
-            if (current_player == PLAYER_1) {
-                board[i][column] = PLAYER_1;
-                break;
-            } else {
-                board[i][column] = PLAYER_2;
-                break;
-            }
+    void Input() override {
+        cin >> user_in;
+        
+        if (user_in >= 0 && user_in <= width) {
+            PlaceToken(user_in);
+        } else {
+            cout << "Enter number between 1 and " << width << endl;
         }
     }
-}
-
-void SwitchPlayer(SlotState &current_player) {
-    if (current_player == PLAYER_1) {
-        current_player = PLAYER_2;
-    } else {
-        current_player = PLAYER_1;
+    
+    int GetUserIn() {
+        return user_in;
     }
-}
+    
+private:
+    void PlaceToken(int column) {
+        column--; // Offset input by 1 to align with desired column
+        
+        // Find first BLANK slot in column to place current player token
+        for (int i = height - 1; i >= 0; i--) {
+            SlotState current_state = board[i][column];
+            if (current_state == BLANK) {
+                if (current_player == PLAYER_1) {
+                    board[i][column] = PLAYER_1;
+                    break;
+                } else {
+                    board[i][column] = PLAYER_2;
+                    break;
+                }
+            }
+        }
+        
+        SwitchPlayer();
+    }
+    
+    void SwitchPlayer() {
+        if (current_player == PLAYER_1) {
+            current_player = PLAYER_2;
+        } else {
+            current_player = PLAYER_1;
+        }
+    }
+};
 
-void QuitGame(int &height, SlotState **&board) {
-    // Deallocate memory
-    for (int i = 0; i < height; i++)
-        delete board[i];
-    delete board;
+int main() {
+    cout << "Welcome to Connect Four!" << endl;
+    ConnectFour ConFour = ConnectFour();
+    
+    do {
+        ConFour.Draw();
+        ConFour.Input();
+    } while (ConFour.GetUserIn());
+    
+    return 0;
 }
